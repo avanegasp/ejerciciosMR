de088e8cc02dd38db24729b31650ba6245e7905b
avanegasp <arco527@gmail.com>
2017-07-13 21:38:41 -0500

    add

--- js/app.js
+++ js/app.js
@@ -2,11 +2,6 @@ var name = $('#first-name').val()
 var lastName = $('#last-name').val()
 var phone = $('#mobile-number').val()
 
-
-$('input').keyup(function(){
-  $(this).append('th:first')
-
-
     // var value = $( this ).val()
 })
 
