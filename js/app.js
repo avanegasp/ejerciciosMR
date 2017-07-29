55990d5ac650b1c234d08de71b1fb865972fa380
avanegasp <arco527@gmail.com>
2017-07-28 23:07:00 -0500

    event keypress ...

--- js/app.js
+++ js/app.js
@@ -1,4 +1,39 @@
-$('.modal-footer').click(function(){
-  $(this).children('button')
-    alert('hola mundo')
-  })
+  // $('form').submit(function(e){
+  //   e.preventDefault();
+  //
+  //   //USUARIO
+  //   var _user = $('#nombre').val()
+  //
+  //     if(_user.indexOf(' ') >= 0){
+  //       $('#nombre').closest('.form-group').addClass('has-error')
+  //     }else if($('#nombre').val().length === 0){
+  //       $('#nombre').closest('.form-group').addClass('has-error')
+  //     }else{
+  //       $('#nombre').closest('.form-group').removeClass('has-error')
+  //     }
+  //
+  //     //CORREO
+  //   var _correo = $('#email').val()
+  //
+  //     if(_correo.indexOf(' ') >= 0){
+  //       $('#email').closest('.form-group').addClass('has-error')
+  //     }else if($('#email').val().length === 0){
+  //       $('#email').closest('.form-group').addClass('has-error')
+  //     }else{
+  //       $('#email').closest('.form-group').removeClass('has-error')
+  //     }
+  //
+  //     //CONTRASEÑA
+  //   var _contraseña = $('#contraseña').val()
+  //
+  //     if(_contraseña.indexOf( ' ' ) >= 0){
+  //       $('#contraseña').closest('.form-group').addClass('has-error')
+  //     }else if($('#contraseña').val().length === 0){
+  //       $('#contraseña').closest('.form-group').addClass('has-error')
+  //     }else{
+  //       $('#contraseña').closest('.form-group').removeClass('has-error')
+  //     }
+  //
+  //
+  //
+  // });
