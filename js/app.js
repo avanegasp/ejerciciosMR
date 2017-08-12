a9f98744dc05c140e228449250a7335460d6ba27
avanegasp <arco527@gmail.com>
2017-08-12 10:37:46 -0500

    Probe2

--- js/app.js
+++ js/app.js
@@ -1,39 +1,10 @@
-  // $('form').submit(function(e){
-  //   e.preventDefault();
-  //
-  //   //USUARIO
-  //   var _user = $('#nombre').val()
-  //
-  //     if(_user.indexOf(' ') >= 0){
-  //       $('#nombre').closest('.form-group').addClass('has-error')
-  //     }else if($('#nombre').val().length === 0){
-  //       $('#nombre').closest('.form-group').addClass('has-error')
-  //     }else{
-  //       $('#nombre').closest('.form-group').removeClass('has-error')
-  //     }
-  //
-  //     //CORREO
-  //   var _correo = $('#email').val()
-  //
-  //     if(_correo.indexOf(' ') >= 0){
-  //       $('#email').closest('.form-group').addClass('has-error')
-  //     }else if($('#email').val().length === 0){
-  //       $('#email').closest('.form-group').addClass('has-error')
-  //     }else{
-  //       $('#email').closest('.form-group').removeClass('has-error')
-  //     }
-  //
-  //     //CONTRASEÑA
-  //   var _contraseña = $('#contraseña').val()
-  //
-  //     if(_contraseña.indexOf( ' ' ) >= 0){
-  //       $('#contraseña').closest('.form-group').addClass('has-error')
-  //     }else if($('#contraseña').val().length === 0){
-  //       $('#contraseña').closest('.form-group').addClass('has-error')
-  //     }else{
-  //       $('#contraseña').closest('.form-group').removeClass('has-error')
-  //     }
-  //
-  //
-  //
-  // });
+$('#btn_enviar').on('click', function(){
+  var input = ($('input:text[name=nombre]').val());
+  $('ul#lista').append('<li>' + input + '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>' +  '</li>')
+});
+
+
+$('#lista').on('click', 'li', function(){
+  var input = ($('input:text[name=nombre]').val());
+  $(this).remove();
+});
