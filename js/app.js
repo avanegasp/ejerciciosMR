8942fce6aca43f2db0187be15a0f8f24c3b829f1
avanegasp <arco527@gmail.com>
2017-08-13 22:02:07 -0500

    Bootstrap issues 2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 
 
-$('#btn-enviar').on('click', function(e){
+$('#btn-enviar').on('submit',function(e){
   e.preventDefault();
+  console.log('hola');
   var input = $('#input-name').val();
-  $('ul#lista').append('<li class="list-group-item">' + input + '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>' +  '</li>')
-  $(input).val('');
-  console.log('hola')
+  $('ul#lista').append('<div class="row justify-content-md-center">'+'<div class="col-md-4">' + '<li class="list-group-item">' + input + '<svg viewBox="0 0 32 32" width="12" height="12" stroke="currentcolor">' + '<path d="M2 30 L30 2 M30 30 L2 2" />' + '</svg>' + '</li>')
 });
 
 
-$('ul#lista').on('click', 'li span', function(){
+$('ul#lista').on('click', 'svg', function(){
   var input = $('#input-name').val();
   $(this).parent().remove();
 });
