6e0a91de2a04f8ca421ef5984204da975bc6e5b6
avanegasp <arco527@gmail.com>
2017-08-14 22:03:18 -0500

    validation list

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
-
-
-$('#btn-enviar').on('click',function(e){
+$('form').on('submit',function(e){
   e.preventDefault();
-  console.log('hola');
   var input = $('#input-name').val();
-  $('ul#lista').append('<div class="row justify-content-md-center">'+'<div class="col-md-4">' + '<li class="list-group-item">' + input + '<svg viewBox="0 0 32 32" width="12" height="12" stroke="currentcolor">' + '<path d="M2 30 L30 2 M30 30 L2 2" />' + '</svg>' + '</li>')
-});
+  $('ul#lista').append('<li class="list-group-item">' + input + '<svg viewBox="0 0 32 32" width="12" height="12" stroke="currentcolor">' + '<path d="M2 30 L30 2 M30 30 L2 2" />' + '</svg>' + '</li>')
+  $('#input-name').val('');
 
+
+  if(input.indexOf(' ') >= 0){
+    $('#input-name').addClass("is-invalid")
+    console.log('primera ')
+  }else if ($('#input-name').val().length === 0){
+    console.log('segunda bn')
+    $('#input-name').addClass("is-invalid")
+  }else{
+    $('#input-name').removeClass("is-invalid")
+    append('<li class="list-group-item">' + input + '<svg viewBox="0 0 32 32" width="12" height="12" stroke="currentcolor">' + '<path d="M2 30 L30 2 M30 30 L2 2" />' + '</svg>' + '</li>')
+    console.log('tercera bn')
+  };
+});
 
 $('ul#lista').on('click', 'svg', function(){
   var input = $('#input-name').val();
