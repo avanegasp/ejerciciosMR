bef9b2b1911e5778355f4e7e9cd887e9e44deb78
avanegasp <arco527@gmail.com>
2017-08-20 11:11:38 -0500

    Invited list

--- js/app.js
+++ js/app.js
@@ -6,14 +6,16 @@ $('form').on('submit',function(e){
 
 
   if(input.indexOf(' ') >= 0){
-    $('#input-name').addClass("is-invalid")
+    $('#input-name').addClass('is-invalid')
     console.log('primera ')
+
   }else if ($('#input-name').val().length === 0){
+    $('#input-name').addClass('is-invalid')
     console.log('segunda bn')
-    $('#input-name').addClass("is-invalid")
+
   }else{
-    $('#input-name').removeClass("is-invalid")
-    append('<li class="list-group-item">' + input + '<svg viewBox="0 0 32 32" width="12" height="12" stroke="currentcolor">' + '<path d="M2 30 L30 2 M30 30 L2 2" />' + '</svg>' + '</li>')
+    $('#input-name').removeClass('is-invalid')
+    // append('<li class="list-group-item">' + input + '<svg viewBox="0 0 32 32" width="12" height="12" stroke="currentcolor">' + '<path d="M2 30 L30 2 M30 30 L2 2" />' + '</svg>' + '</li>')
     console.log('tercera bn')
   };
 });
