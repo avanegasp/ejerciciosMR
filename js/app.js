3249ac3709ed40204c8ff1235caf0daac9a73f4a
avanegasp <arco527@gmail.com>
2017-09-08 09:59:35 -0500

    Countries with AJAX

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-$( "div" ).click(function() {
-  var html = [ "The clicked div has the following styles:" ];
+$('button').on('click', function(){
 
-  var styleProps = $( this ).css([
-    "width", "height", "color", "background-color"
-  ]);
-  $.each( styleProps, function( prop, value ) {
-    html.push( prop + ": " + value );
-  });
+  $.get("https://s3.amazonaws.com/makeitreal/countries.json", function(countries){
 
-  $( "#result" ).html( html.join( "<br>" ) );
+    $('body').append('<table></table>');
+    $('table').append('<tr><th>' +"Code" + '</th>' + '<th>' + "Name" + '</th>' + '<th>' +"Flag"+ '</th>' +'</tr>')
+    $('table').append('<tr><td>' + countries[0].code + '</td><td>' + countries[0].name + '</td><td><img src="' + countries[0].flag_url + '"></td></tr>')
+    $('table').append('<tr><td>' + countries[1].code + '</td><td>' + countries[1].name + '</td><td><img src="' + countries[1].flag_url + '"></td></tr>')
+    $('table').append('<tr><td>' + countries[2].code + '</td><td>' + countries[2].name + '</td><td><img src="' + countries[2].flag_url + '"></td></tr>')
+    $('table').append('<tr><td>' + countries[3].code + '</td><td>' + countries[3].name + '</td><td><img src="' + countries[3].flag_url + '"></td></tr>')
+
+    $('button').hide();
+  })
 });
