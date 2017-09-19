1d38dc1939372817dd6893b2fde8dc49e4692f9a
avanegasp <arco527@gmail.com>
2017-09-18 22:13:05 -0500

    Add song only with Js

--- js/app.js
+++ js/app.js
@@ -1,48 +1,13 @@
-//
-// $('li').click(function(evento){
-//   var $tab = $(evento.currentTarget)  //elemento al cúal le hice click (li), es un objeto jquery
-//   $('.tabs').children('li').removeClass('active')
-//   $tab.addClass('active')
-//
-//   var hrefAttr = $tab.children('a').attr('href') //nombre del atributo
-//   console.log(hrefAttr)
-//   $('.tabs-content').children('div').removeClass('active')
-//   $(hrefAttr).addClass('active')
-// })
-//
-// $('li').click(function(evento){
-//   var li = $(evento.currentTarget)
-//   var liAttr = li.attr('data-section')
-//   console.log(liAttr)
-//
-// $('section').removeClass('show')
-// $('section').addClass('show')
-//
-// })
+var songs = [
+  {
+    number: 1, name: "Camila Cabello - Havana",url: "https://www.youtube.com/embed/HCjNJDNzw8Y"},
+  {
+    number: 2, name: "Justin Bieber - Friends",url: "https://www.youtube.com/embed/ctfLLsD84vk"},
+  {
+    number: 3, name: "Taylor Swift - Look What You Make Me Do",url: "https://www.youtube.com/embed/3tmd-ClpJxA"}
+];
 
-// $('button').click(function(){
-//   $('section').addClass('show')
-// })
-
-
-$('li').click(function(evento){
-  var li = $(evento.currentTarget)
-  var liAttr = li.attr('data-section')
-  console.log(liAttr)
-
-  $('section').removeClass('show')
-  $(liAttr).addClass('show')  
-})
-
-
-
-
-
-
-
-
-//basicamente lo que va pasar es que se va disparar un evento `click` sobre una de las pestañas
-// tienes que crear ese evento
-// cueando ese evento suceda tienes que ponerle la clase
-// `active` a la pestaña que le hicieron click y quitarsela a la que antes lo
-// tenia que es lo mismo que decir quitarsela a todas las demas
+ $('body').append('<table></table>');
+ $('tbody').append('<tr><td>'+songs[0].number+'</td><td>'+songs[0].name+'</td><td><iframe src="' + songs[0].url + '"></td></tr>')
+ $('tbody').append('<tr><td>'+songs[1].number+'</td><td>'+songs[1].name+'</td><td><iframe src="' + songs[1].url + '"></td></tr>')
+ $('tbody').append('<tr><td>'+songs[2].number+'</td><td>'+songs[2].name+'</td><td><iframe src="' + songs[2].url + '"></td></tr>')
