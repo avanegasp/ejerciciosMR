b0d32038c334e1c37a39b275178f2497d60cf6cc
avanegasp <arco527@gmail.com>
2017-09-20 11:50:43 -0500

    Add other way to make the things

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-// console.log( resultado)
-
+  //
+  //
   // var body = document.body;
   // var h1 = document.createElement("h1");
   // h1.innerHTML = "Prueba de listas";
@@ -14,36 +14,61 @@
   // var list1 = document.createElement("ul");
   // for(i=0; i < num.length; i++){
   //   var number = num[i];
-  //   console.log(number)
   //       if(number > 10){
   //         resultado.push(number)
   //       }
   //     var li = document.createElement("li");
-  //      console.log(li)
+  //
   //     li.innerHTML = "elemento #" + resultado;
   //     list1.appendChild(li);
   // }
-  //
+  // console.log(resultado)
   // body.appendChild(h1);
-  // console.log(h1)
   // body.appendChild(titleUl);
-  // console.log(titleUl)
   // body.appendChild(list1);
-  // console.log(list1)
 
 
-  var body = document.body;
-  var h1 = document.createElement('h5');
-  h1.innerHTML= "parrafos con libros";
-  body.appendChild(h1)
+  //
+  // var body = document.body;
+  // var h1 = document.createElement('h5');
+  // h1.innerHTML= "parrafos con libros";
+  // body.appendChild(h1)
+  //
+  //
+  // var libros = ['libro1', 'libro2']
+  //
+  // for (i=0; i < libros.length; i++){
+  //   var libro = libros[i]
+  //
+  //   var parrafos = document.createElement('p')
+  //     parrafos.innerHTML = libro
+  //     body.appendChild(parrafos)
+  // }
 
 
-  var libros = ['libro1', 'libro2']
 
-  for (i=0; i < libros.length; i++){
-    var libro = libros[i]
+  var body = document.body
+  var ulList = document.createElement('ul')
+  var num = [22, 3, 55, 2, 11, 9, 5, 27]
 
-    var parrafos = document.createElement('p')
-      parrafos.innerHTML = libro
-      body.appendChild(parrafos)
+  for (i=0; i < num.length; i++){
+    if(num[i] > 10){
+
+  var liList = document.createElement('li')
+      liList.innerHTML = num[i];
+      body.appendChild(liList);
   }
+};
+
+  var body = document.body
+    var ulList = document.createElement('ul')
+    var num = [22, 3, 55, 2, 11, 9, 5, 27]
+    var liList = '';
+
+    for (i=0; i < num.length; i++) {
+      if (num[i] > 10) {
+        liList = liList.concat('<li>'+num[i]+'</li>');
+      }
+  };
+  ulList.innerHTML = liList;
+  body.append(ulList)
