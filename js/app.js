0878a1f34322d948baabfdbd350f7732d1f468da
avanegasp <arco527@gmail.com>
2017-09-21 17:28:01 -0500

    Using parseInt, Math, push

--- js/app.js
+++ js/app.js
@@ -1,28 +1,50 @@
 
+    var arr = []
 
-  $('#add').on('click', function(){
-    var input = $('input').val()
-      if(input === ''){
-        $('.result').hide()
-      }else{
-        $('.result').show()
-        $('#max').show()
-      }
-  })
-
-  $('#number').change('click', function() {
-    var greaterNumber = $('span').text( $(this).val() );
-        console.log(greaterNumber)
-    });
+    $('#add').on('click', function(){
+      var valInput = $('input').val()
+      console.log (parseInt(valInput))
+      arr.push(valInput)
+      $('input').val('')
+      console.log(arr);
+    })
 
 
+    $('#greater').on('click', function(){
+      var max_of_array = Math.max.apply(Math, arr)
+        alert(max_of_array)
+    })
 
-// var array = [4, 6, 7, 33, 55, 64]
-// //
-// // for (var i=0; i < arr.length; i++){
-// //
-// // }
-//
-// var max_of_array = Math.max.apply(Math, array);
-//
-// console.log(max_of_array)
+
+
+
+
+
+
+
+
+
+
+
+    // $('#add').on('click', function(){
+    //   var input = $('input').val()
+    //     if(input === ''){
+    //       $('.result').hide()
+    //     }else{
+    //       $('.result').show()
+    //     }
+    // })
+    // $('#number').change('click', function() {
+    //   greaterNumber = $('span').text( $(this).val() );
+    //
+    //   // alert(max_of_array)
+    //
+    //   });
+
+
+
+    // for(var i = 0; i < arr.length; i++){
+    //   if(arr[i] > 100)
+    //   alert(arr[i])
+    // }
+    //
