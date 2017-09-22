4625115766ac4b959bddd0c87cbb6c1d9a6de4c9
avanegasp <arco527@gmail.com>
2017-09-22 16:15:44 -0500

    Top5 showing video in HTML but made in Js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,13 @@
+var songs = [
+  {
+    number: 1, name: "Camila Cabello - Havana",url: "https://www.youtube.com/embed/HCjNJDNzw8Y"},
+  {
+    number: 2, name: "Justin Bieber - Friends",url: "https://www.youtube.com/embed/ctfLLsD84vk"},
+  {
+    number: 3, name: "Taylor Swift - Look What You Make Me Do",url: "https://www.youtube.com/embed/3tmd-ClpJxA"}
+];
 
-    var arr = []
-
-    $('#add').on('click', function(){
-      var valInput = $('input').val()
-      arr.push(valInput)
-      $('input').val('')
-      // console.log(arr);
-      if(valInput === ''){
-        $('.result').hide()
-      }else{
-        $('.result').show()
-      }
-  });
-
-    $('#max').on('click', function(){
-      var max_of_array = Math.max.apply(Math, arr)
-        alert(max_of_array)
-    });
-
-    $('#add').on('click', function() {
-      $('span').text(arr);
-      console.log(arr)
-});
+ $('body').append('<table></table>');
+ $('tbody').append('<tr><td>'+songs[0].number+'</td><td>'+songs[0].name+'</td><td><iframe src="' + songs[0].url + '"></td></tr>')
+ $('tbody').append('<tr><td>'+songs[1].number+'</td><td>'+songs[1].name+'</td><td><iframe src="' + songs[1].url + '"></td></tr>')
+ $('tbody').append('<tr><td>'+songs[2].number+'</td><td>'+songs[2].name+'</td><td><iframe src="' + songs[2].url + '"></td></tr>')
