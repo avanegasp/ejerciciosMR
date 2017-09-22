89719e472d25f7f32781016c22a84faf286c42ac
avanegasp <arco527@gmail.com>
2017-09-22 18:00:07 -0500

    Other ways to make HTML with iframe, style

--- js/app.js
+++ js/app.js
@@ -8,6 +8,58 @@ var songs = [
 ];
 
  $('body').append('<table></table>');
- $('tbody').append('<tr><td>'+songs[0].number+'</td><td>'+songs[0].name+'</td><td><iframe src="' + songs[0].url + '"></td></tr>')
- $('tbody').append('<tr><td>'+songs[1].number+'</td><td>'+songs[1].name+'</td><td><iframe src="' + songs[1].url + '"></td></tr>')
- $('tbody').append('<tr><td>'+songs[2].number+'</td><td>'+songs[2].name+'</td><td><iframe src="' + songs[2].url + '"></td></tr>')
+ $('tbody').append('<tr><td>'+songs[0].number+'</td><td>'+songs[0].name+'</td></tr>')
+ $('tbody').append('<tr><td>'+songs[1].number+'</td><td>'+songs[1].name+'</td></tr>')
+ $('tbody').append('<tr><td>'+songs[2].number+'</td><td>'+songs[2].name+'</td></tr>')
+
+
+  $('td').on('click', function(){
+    $('table').hide()
+    var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "https://www.youtube.com/embed/HCjNJDNzw8Y");
+        ifrm.style.width = "640px";
+        ifrm.style.height = "480px";
+        document.body.appendChild(ifrm);
+    })
+
+
+
+
+ //
+ // var body = document.body;
+ // //   var h1 = document.createElement("h1");
+ // //   h1.innerHTML = "Prueba de listas";
+ //   // lista no ordenada
+ //  //  var hul = document.createElement("h3");
+ //  //  hul.innerHTML = "Lista no ordenada";
+ //   var list1 = document.createElement("ul");
+ //   for(i=0;i<10;i++){
+ //       var li = document.createElement("li");
+ //       li.innerHTML = "elemento #"+i;
+ //       list1.appendChild(li);
+ //   }
+ //   console.log(list1)
+ //  //  body.appendChild(h1);
+ //  //  body.appendChild(hul);
+
+
+
+
+
+
+
+
+
+
+
+
+ //
+ //
+ // var videos = [
+ //   {
+ //
+ //     // <td><iframe src="' + songs[0].url + '"></td>
+ //     // <td><iframe src="' + songs[1].url + '"></td>
+ //     // <td><iframe src="' + songs[2].url + '"></td>
+ //   }
+ // ]
