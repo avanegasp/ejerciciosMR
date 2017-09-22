82711aca0e6c12fdc5b93a8693c56eca28c43ab3
avanegasp <arco527@gmail.com>
2017-09-22 14:34:15 -0500

    Exercise max number done

--- js/app.js
+++ js/app.js
@@ -3,48 +3,22 @@
 
     $('#add').on('click', function(){
       var valInput = $('input').val()
-      console.log (parseInt(valInput))
       arr.push(valInput)
       $('input').val('')
-      console.log(arr);
-    })
+      // console.log(arr);
+      if(valInput === ''){
+        $('.result').hide()
+      }else{
+        $('.result').show()
+      }
+  });
 
-
-    $('#greater').on('click', function(){
+    $('#max').on('click', function(){
       var max_of_array = Math.max.apply(Math, arr)
         alert(max_of_array)
-    })
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-    // $('#add').on('click', function(){
-    //   var input = $('input').val()
-    //     if(input === ''){
-    //       $('.result').hide()
-    //     }else{
-    //       $('.result').show()
-    //     }
-    // })
-    // $('#number').change('click', function() {
-    //   greaterNumber = $('span').text( $(this).val() );
-    //
-    //   // alert(max_of_array)
-    //
-    //   });
-
-
-
-    // for(var i = 0; i < arr.length; i++){
-    //   if(arr[i] > 100)
-    //   alert(arr[i])
-    // }
-    //
+    $('#add').on('click', function() {
+      $('span').text(arr);
+      console.log(arr)
+});
